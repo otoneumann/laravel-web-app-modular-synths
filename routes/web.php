66cd9fb0657d75c46d439e395956e7392ed61dcf
otoneumann <otarshenge@gmail.com>
2025-10-27 22:27:12 +0100

    ADD:Routs Modules

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\SynthModuleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('modules', SynthModuleController::class);
