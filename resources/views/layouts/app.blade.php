c26f4d25fe31b66989db9f48d7cd6c4cd563a865
otoneumann <otarshenge@gmail.com>
2025-10-27 22:27:47 +0100

    ADD:View layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Modular Synth App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<div class="container">
+    @yield('content')
+</div>
+</body>
+</html>
