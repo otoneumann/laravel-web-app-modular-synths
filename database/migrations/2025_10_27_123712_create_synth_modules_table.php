2f9ba579ba514c03d48fea921edb2a320b67a870
otoneumann <otarshenge@gmail.com>
2025-10-27 14:23:01 +0100

    ADD: DB Tables

---
+++ database/migrations/2025_10_27_123712_create_synth_modules_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('synth_modules', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('image_url')->nullable();
+            $table->string('api_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('synth_modules');
+    }
+};
---
+++ database/migrations/2025_10_27_124532_create_collection_modules_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collection_modules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('collection_id')->constrained()->onDelete('cascade');
+            $table->foreignId('synth_module_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collection_modules');
+    }
+};
